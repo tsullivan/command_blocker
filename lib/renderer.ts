880f7b01a986b64d903e49391d681b3012a88b22
Timothy Sullivan <tsullivan@elastic.co>
2020-12-05 22:49:00 -0700

    reorg

--- lib/renderer.ts
+++ lib/renderer.ts
@@ -10,7 +10,6 @@ export const renderFactory = (
   ImprovedNoise: typeof THImprovedNoise
 ) => {
   return class RenderProvider {
-    private container: Element;
     private camera: THREE.PerspectiveCamera;
     private controls: any;
     private scene: THREE.Scene;
@@ -22,8 +21,7 @@ export const renderFactory = (
     private data: number[];
     private clock: THREE.Clock;
 
-    constructor(gameContainer: MutableRefObject<Element>) {
-      this.container = gameContainer.current;
+    constructor(private container: Element) {
       this.data = this.generateHeight(this.worldWidth, this.worldDepth);
       this.clock = new THREE.Clock();
 
@@ -37,8 +35,14 @@ export const renderFactory = (
     }
 
     private init() {
-      this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 20000);
-      this.camera.position.y = this.getY(this.worldHalfWidth, this.worldHalfDepth) * 100 + 100;
+      this.camera = new THREE.PerspectiveCamera(
+        60,
+        window.innerWidth / window.innerHeight,
+        1,
+        20000
+      );
+      this.camera.position.y =
+        this.getY(this.worldHalfWidth, this.worldHalfDepth) * 100 + 100;
 
       this.scene = new THREE.Scene();
       this.scene.background = new THREE.Color(0xbfd1e5);
@@ -56,13 +60,13 @@ export const renderFactory = (
       const nxGeometry = new THREE.PlaneBufferGeometry(100, 100);
       (nxGeometry.attributes.uv.array as Array<number>)[1] = 0.5;
       (nxGeometry.attributes.uv.array as Array<number>)[3] = 0.5;
-      nxGeometry.rotateY(- Math.PI / 2);
-      nxGeometry.translate(- 50, 0, 0);
+      nxGeometry.rotateY(-Math.PI / 2);
+      nxGeometry.translate(-50, 0, 0);
 
       const pyGeometry = new THREE.PlaneBufferGeometry(100, 100);
       (pyGeometry.attributes.uv.array as Array<number>)[5] = 0.5;
       (pyGeometry.attributes.uv.array as Array<number>)[7] = 0.5;
-      pyGeometry.rotateX(- Math.PI / 2);
+      pyGeometry.rotateX(-Math.PI / 2);
       pyGeometry.translate(0, 50, 0);
 
       const pzGeometry = new THREE.PlaneBufferGeometry(100, 100);
@@ -74,16 +78,14 @@ export const renderFactory = (
       (nzGeometry.attributes.uv.array as Array<number>)[1] = 0.5;
       (nzGeometry.attributes.uv.array as Array<number>)[3] = 0.5;
       nzGeometry.rotateY(Math.PI);
-      nzGeometry.translate(0, 0, - 50);
+      nzGeometry.translate(0, 0, -50);
 
       //
 
       const geometries = [];
 
       for (let z = 0; z < this.worldDepth; z++) {
-
         for (let x = 0; x < this.worldWidth; x++) {
-
           const h = this.getY(x, z);
 
           matrix.makeTranslation(
@@ -100,40 +102,35 @@ export const renderFactory = (
           geometries.push(pyGeometry.clone().applyMatrix4(matrix));
 
           if ((px !== h && px !== h + 1) || x === 0) {
-
             geometries.push(pxGeometry.clone().applyMatrix4(matrix));
-
           }
 
           if ((nx !== h && nx !== h + 1) || x === this.worldWidth - 1) {
-
             geometries.push(nxGeometry.clone().applyMatrix4(matrix));
-
           }
 
           if ((pz !== h && pz !== h + 1) || z === this.worldDepth - 1) {
-
             geometries.push(pzGeometry.clone().applyMatrix4(matrix));
-
           }
 
           if ((nz !== h && nz !== h + 1) || z === 0) {
-
             geometries.push(nzGeometry.clone().applyMatrix4(matrix));
-
           }
-
         }
-
       }
 
       const geometry = BufferGeometryUtils.mergeBufferGeometries(geometries);
       geometry.computeBoundingSphere();
 
-      const texture = new THREE.TextureLoader().load('textures/minecraft/atlas.png');
+      const texture = new THREE.TextureLoader().load(
+        "textures/minecraft/atlas.png"
+      );
       texture.magFilter = THREE.NearestFilter;
 
-      const mesh = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({ map: texture, side: THREE.DoubleSide }));
+      const mesh = new THREE.Mesh(
+        geometry,
+        new THREE.MeshLambertMaterial({ map: texture, side: THREE.DoubleSide })
+      );
       this.scene.add(mesh);
 
       const ambientLight = new THREE.AmbientLight(0xcccccc);
@@ -148,7 +145,10 @@ export const renderFactory = (
       this.renderer.setSize(window.innerWidth, window.innerHeight);
       this.container.appendChild(this.renderer.domElement);
 
-      this.controls = new FirstPersonControls(this.camera, this.renderer.domElement);
+      this.controls = new FirstPersonControls(
+        this.camera,
+        this.renderer.domElement
+      );
 
       this.controls.movementSpeed = 1000;
       this.controls.lookSpeed = 0.125;
@@ -156,70 +156,76 @@ export const renderFactory = (
 
       //
 
-      window.addEventListener('resize', this.onWindowResize, false);
-
+      window.addEventListener("resize", this.onWindowResize, false);
     }
 
     private onWindowResize() {
-
       this.camera.aspect = window.innerWidth / window.innerHeight;
       this.camera.updateProjectionMatrix();
 
       this.renderer.setSize(window.innerWidth, window.innerHeight);
 
       this.controls.handleResize();
-
     }
 
     private generateHeight(width: number, height: number) {
-
-      const data = [], perlin = new ImprovedNoise(),
-        size = width * height, z = Math.random() * 100;
+      const data = [],
+        perlin = new ImprovedNoise(),
+        size = width * height,
+        z = Math.random() * 100;
 
       let quality = 2;
 
       for (let j = 0; j < 4; j++) {
-
         if (j === 0) for (let i = 0; i < size; i++) data[i] = 0;
 
         for (let i = 0; i < size; i++) {
-
-          const x = i % width, y = (i / width) | 0;
+          const x = i % width,
+            y = (i / width) | 0;
           data[i] += perlin.noise(x / quality, y / quality, z) * quality;
-
-
         }
 
         quality *= 4;
-
       }
 
       return data;
-
     }
 
     private getY(x: number, z: number) {
-
       return (this.data[x + z * this.worldWidth] * 0.2) | 0;
-
     }
 
     //
 
     private animate() {
-
       requestAnimationFrame(this.animate);
 
       this.render();
-
     }
 
     private render() {
-
       this.controls.update(this.clock.getDelta());
       this.renderer.render(this.scene, this.camera);
-
     }
-
   };
+};
+
+export const useRenderer = (container: Element) => {
+  // load imgs
+
+  Promise.all([
+    import("three/examples/jsm/utils/BufferGeometryUtils"),
+    import("three/examples/jsm/controls/FirstPersonControls"),
+    import("three/examples/jsm/math/ImprovedNoise"),
+  ]).then(
+    ([{ BufferGeometryUtils }, { FirstPersonControls }, { ImprovedNoise }]) => {
+      const RenderProvider = renderFactory(
+        BufferGeometryUtils,
+        FirstPersonControls,
+        ImprovedNoise
+      );
+      const r = new RenderProvider(container);
+      // r.useRenderer();
+    }
+  );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,31 @@
+/*
+ Whatcha gonna do when you get outta jail?
+ I'm gonna have some lunch
+
+ A Game by Henry
+*/
+
 import Head from "next/head";
 import { useEffect, useRef } from "react";
-import { renderFactory } from "../lib/renderer";
-
-/*
- A Game by Henry
-
- Q: Whatcha gonna do when you get outta jail?
- A: I'm gonna have some lunch
-*/
+import { useRenderer } from "../lib/renderer";
 
 export default function Home() {
   const gameContainer = useRef(null);
 
   useEffect(() => {
-    Promise.all([
-      import("three/examples/jsm/utils/BufferGeometryUtils"),
-      import("three/examples/jsm/controls/FirstPersonControls"),
-      import("three/examples/jsm/math/ImprovedNoise"),
-    ]).then(
-      ([
-        { BufferGeometryUtils },
-        { FirstPersonControls },
-        { ImprovedNoise },
-      ]) => {
-        const RenderProvider = renderFactory(
-          BufferGeometryUtils,
-          FirstPersonControls,
-          ImprovedNoise
-        );
-        const r = new RenderProvider(gameContainer);
-        r.useRenderer();
-      }
-    );
+    useRenderer(gameContainer.current);
   });
 
   return (
     <>
-      {}
       <Head>
         <title>Command Blocker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="container">
-        <main>
-          <div ref={gameContainer}></div>
-        </main>
-      </div>
+      <main>
+        <div ref={gameContainer}></div>
+      </main>
 
       <style jsx>{`
         main {
